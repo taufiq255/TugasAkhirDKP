9eafedad4b438ed7c7189c2d554d5b0f30cfa322
taufiq255 <64286433+taufiq255@users.noreply.github.com>
2020-05-15 20:46:00 +0700

    Add files via upload

---
+++ Transaksi Penjualan/src/transaksi/penjualan/TransaksiPenjualan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transaksi.penjualan;
+
+/**
+ *
+ * @author TAUFIQ
+ */
+public class TransaksiPenjualan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
